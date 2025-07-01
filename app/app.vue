4fc6e44e04287f417f99c4c194b5905d22f2468c
Zenara12 <nolipernes@gmail.com>
2025-07-01 23:09:02 +0800

    update: google search verification meta

--- app/app.vue
+++ app/app.vue
@@ -6,6 +6,7 @@ useHead({
   titleTemplate: `%s ${title}`,
   meta: [
     { name: 'keywords', content: '%s dish, recipes, cooking, food, home cooking, easy recipes' },
+    { name: 'google-site-verification', content: 'ePPm5orCDaKbmzGwisryY4GGZkcHfqYLREK0V56pz0I' }
   ]
 })
 
